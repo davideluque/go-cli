a146cd593502590e39fc4ee3c1126238fdd5fc15
David Cabeza <cabezadavide@gmail.com>
2022-07-30 09:58:15 +0200

    First implementation of flag parsing

--- main.go
+++ main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
+	"flag";
 	"fmt"
 )
 
 func main() {
-	fmt.Println("Hello, World!")
+	dice := flag.String("d", "d6", "dice to roll. e.g. d6, d10, d20, etc. default is d6")
+
+	flag.Parse()
+	
+	fmt.Println("Rolling", *dice)
 }
