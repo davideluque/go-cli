d866624565c2681c3f0e041d13fe7aa43302e716
David Cabeza <cabezadavide@gmail.com>
2022-07-30 10:07:02 +0200

    Adds regex to validate input & get sides

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
-	"flag";
+	"flag"
 	"fmt"
+	"regexp"
 )
 
 func main() {
 	dice := flag.String("d", "d6", "dice to roll. e.g. d6, d10, d20, etc. default is d6")
 
 	flag.Parse()
-	
-	fmt.Println("Rolling", *dice)
+
+	matched, _ := regexp.MatchString("d[0-9]+", *dice)
+
+	if matched {
+		diceSides := regexp.MustCompile("[0-9]+").FindString(*dice)
+		fmt.Println(diceSides)
+	} else {
+		fmt.Println("Invalid dice")
+	}
 }
