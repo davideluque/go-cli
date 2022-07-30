72f487d16b54522ecdc61ef114cb676bb01b94fc
David Cabeza <cabezadavide@gmail.com>
2022-07-30 10:15:04 +0200

    Roll the dice. Seed the rand.

--- main.go
+++ main.go
@@ -3,10 +3,16 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math/rand"
 	"regexp"
+	"strconv"
+	"time"
 )
 
 func main() {
+	// seed the random number generator
+	rand.Seed(time.Now().UnixNano())
+
 	dice := flag.String("d", "d6", "dice to roll. e.g. d6, d10, d20, etc. default is d6")
 
 	flag.Parse()
@@ -15,8 +21,15 @@ func main() {
 
 	if matched {
 		diceSides := regexp.MustCompile("[0-9]+").FindString(*dice)
-		fmt.Println(diceSides)
+
+		// Cast diceSides to int and roll the dice
+		sides, _ := strconv.Atoi(diceSides)
+
+		roll := rand.Intn(sides) + 1
+
+		fmt.Println("You rolled a", roll)
 	} else {
 		fmt.Println("Invalid dice")
+		fmt.Println("Usage: dice -d d6")
 	}
 }
